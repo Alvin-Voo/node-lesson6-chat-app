c9e772d60e9f9b8eafd812ce35121f65b652d01b
Alvin Voo <alvin.voo@gmail.com>
2018-05-15 15:53:04 +0800

    added css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
+  let messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage',{
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function(){
-
+    messageTextBox.val('')
   })
 })
 
@@ -43,14 +45,18 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported by your browser.')
   }
 
+  locationButton.attr('disabled',true).text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position){
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
+    locationButton.removeAttr('disabled').text('Send location');
   }, function(e){
     console.log(e);
-    alert('Unable to fetch location')
+    alert('Unable to fetch location');
+    locationButton.removeAttr('disabled').text('Send location');
   })
 
 })
